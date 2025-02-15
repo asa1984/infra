66a89ff80a4dd9976fdfcfc19ee4092d86eaf522
asa1984 <satoasa9913@gmail.com>
2025-02-15 16:13:31 +0900

    fix: `github_repository.infra`を`data.github_repository.infra`に変更
    GitHub Appの権限ではリポジトリの作成ができないため

--- terraform/github_repositories.tf
+++ terraform/github_repositories.tf
@@ -1,25 +1,13 @@
 # asa1984/infra
 
-resource "github_repository" "infra" {
-  name        = "infra"
-  description = "INFRASTRUCTURE"
-  visibility  = "public"
-
-  allow_auto_merge       = true
-  allow_merge_commit     = true
-  allow_rebase_merge     = false
-  allow_update_branch    = false
-  delete_branch_on_merge = true
-
-  has_discussions = false
-  has_projects    = false
-  has_wiki        = false
+data "github_repository" "infra" {
+  full_name = "asa1984/infra"
 }
 
 resource "github_repository_ruleset" "infra_main" {
   enforcement = "active"
   name        = "mainブランチの保護"
-  repository  = github_repository.infra.name
+  repository  = data.github_repository.infra.name
   target      = "branch"
 
   conditions {
@@ -31,11 +19,6 @@ resource "github_repository_ruleset" "infra_main" {
 
   rules {
     deletion = false
-
-    branch_name_pattern {
-      operator = "starts_with"
-      pattern  = "main"
-    }
 
     pull_request {
       required_approving_review_count = 1
