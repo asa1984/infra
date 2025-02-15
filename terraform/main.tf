30e4387b910d2f5ec31a07492c131a707b9a9518
asa1984 <satoasa9913@gmail.com>
2025-02-15 16:12:08 +0900

    feat: Terraformのbackendを設定

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,19 @@ terraform {
       source = "registry.opentofu.org/vultr/vultr"
     }
   }
+
+  backend "s3" {
+    bucket = "tfstate"
+    key    = "asa1984/infra/terraform"
+    region = "auto"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
 }
 
 # GitHub
